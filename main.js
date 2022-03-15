7e6d28970faaf03564b8e5423bba4c756a2b9890
TuhinSa <92089255+TuhinSa@users.noreply.github.com>
2022-03-15 16:56:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({
+        audio : true
+    })
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/VRUWxrJZ2/model.json", modelReady);
+    
+}
+function modelReady(){
+    classifier.classify(gotResults);
+    
+}
+function gotResults(error, results){
+    console.log("Got Result")
+}
